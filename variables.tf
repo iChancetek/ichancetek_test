c0caae26eb140f17715e9992a85aa7333d43b718
iChancetek <devops@ichancetek.com>
2023-01-06 13:33:30 -0500

    first commit

---
+++ variables.tf
@@ -0,0 +1,31 @@
+
+variable client_id {}
+variable client_secret {}
+variable ssh_public_key {}
+
+
+# variable environment {
+#     default = "dev"
+# }
+
+# variable location {
+#     default = "East US"
+# }
+
+# variable node_count {
+#   default = 2
+# }
+
+
+
+# variable dns_prefix {
+#   default = "k8stest"
+# }
+
+# variable cluster_name {
+#   default = "k8stest"
+# }
+
+# variable resource_group {
+#   default = "kubernetes"
+# }
